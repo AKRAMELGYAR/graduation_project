5e63918ae3f7d3260988a16f26d56d829b12ae04
ziad <ziaddash33@gmail.com>
2025-03-03 17:55:20 +0200

    fix some issues

--- Auth/repo/authRepo.js
+++ Auth/repo/authRepo.js
@@ -1,7 +1,7 @@
 import {User} from "../../user/model/userModel.js";
 
 export const findUser = async ({payload}) => {
-    return await User.findOne(payload).select('-password -__v -role');
+    return await User.findOne(payload).select(' -__v -role');
 }
 
 export const saveUser = async ({ userData }) => {
