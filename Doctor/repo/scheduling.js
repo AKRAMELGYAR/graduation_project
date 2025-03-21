c21b6aca5a27d5f651c00b5dfbab58063e76e6bc
ziad <ziaddash33@gmail.com>
2025-03-22 00:18:32 +0200

    Fix Duplicated slots issue

--- Doctor/repo/scheduling.js
+++ Doctor/repo/scheduling.js
@@ -5,3 +5,20 @@ import scheduleModel from "../model/scheduleModel.js";
 export const setSchedule = async (availableSlots) => {
     return await scheduleModel.insertMany(availableSlots);
 }
+
+export const updateSchedule = async ({ payload, availableSlots }) => {
+
+    let updated = null;
+    for (const slots of availableSlots) {
+        updated = await scheduleModel.updateMany(
+                payload, 
+                { $set: { slots: slots.slots } },
+        );
+    }
+
+    return updated;
+};
+
+export const getScheule = async ({payload}) => {
+    return await scheduleModel.find(payload);
+}
--- Doctor/services/scheduleService.js
+++ Doctor/services/scheduleService.js
@@ -1,13 +1,14 @@
 import { DateTime } from "luxon";
-import { setSchedule } from "../repo/scheduling.js";
-
+import { getScheule, setSchedule, updateSchedule } from "../repo/scheduling.js";
+import { generating } from "./generatingFun.js";
 
 export const generateWeeklySlots = async (doctorId, workingDays, sessionDuration) => {
     const today = DateTime.now().startOf("day");
     const startOfWeek = today.startOf("week"); 
-    let schedules = {}; 
+    let availableSlots = [];
 
     for (const { day, slots } of workingDays) {
+
         let targetDate = startOfWeek.plus({ days: DateTime.fromFormat(day, "EEEE").weekday - 1 });
 
         if (targetDate < today) {
@@ -15,46 +16,79 @@ export const generateWeeklySlots = async (doctorId, workingDays, sessionDuration
             // continue; 
         }
 
-        const key = `${doctorId}-${day}-${targetDate.toISODate()}`;
+        const findSchedule = await getScheule({ payload: { doctorId, date: targetDate.toISODate() } });
 
-        if (!schedules[key]) {
-            schedules[key] = {
-                doctorId,
-                day,
-                date: targetDate.toISODate(),
-                slots: []
-            };
-        }
+        if (findSchedule.length === 0) {
+            const newSlots = await generating({ DateTime, doctorId, targetDate, slots, day, sessionDuration });
+            availableSlots.push(newSlots);
+            await setSchedule(availableSlots);
 
-        for (const { startTime, endTime } of slots) {
-            let start = targetDate.set({ 
-                hour: DateTime.fromFormat(startTime, "hh:mm a").hour, 
-                minute: DateTime.fromFormat(startTime, "hh:mm a").minute 
+        } else {
+            const newSlots = await generating({ DateTime, doctorId, targetDate, slots, day, sessionDuration });
+            availableSlots.push(newSlots);
+            await updateSchedule({ 
+                payload: { doctorId, date: targetDate.toISODate() }, 
+                availableSlots 
             });
-
-            let end = targetDate.set({ 
-                hour: DateTime.fromFormat(endTime, "hh:mm a").hour, 
-                minute: DateTime.fromFormat(endTime, "hh:mm a").minute 
-            });
-
-            while (start < end) {
-                let slotEnd = start.plus({ minutes: sessionDuration });
-
-                schedules[key].slots.push({
-                    startTime: start.toFormat("hh:mm a"),
-                    endTime: slotEnd.toFormat("hh:mm a"),
-                    isBooked: false
-                });
-
-                start = slotEnd;
-            }
         }
     }
 
-    
-    const availableSlots = Object.values(schedules);
-
-
-    await setSchedule(availableSlots);
     return availableSlots;
 };
+
+// export const generateWeeklySlots = async (doctorId, workingDays, sessionDuration) => {
+//     const today = DateTime.now().startOf("day");
+//     const startOfWeek = today.startOf("week"); 
+//     let schedules = {}; 
+
+//     for (const { day, slots } of workingDays) {
+//         let targetDate = startOfWeek.plus({ days: DateTime.fromFormat(day, "EEEE").weekday - 1 });
+
+//         if (targetDate < today) {
+//             targetDate = targetDate.plus({ days: 7 });
+//             // continue; 
+//         }
+
+//         const key = `${doctorId}-${day}-${targetDate.toISODate()}`;
+
+//         if (!schedules[key]) {
+//             schedules[key] = {
+//                 doctorId,
+//                 day,
+//                 date: targetDate.toISODate(),
+//                 slots: []
+//             };
+//         }
+
+//         for (const { startTime, endTime } of slots) {
+//             let start = targetDate.set({ 
+//                 hour: DateTime.fromFormat(startTime, "hh:mm a").hour, 
+//                 minute: DateTime.fromFormat(startTime, "hh:mm a").minute 
+//             });
+
+//             let end = targetDate.set({ 
+//                 hour: DateTime.fromFormat(endTime, "hh:mm a").hour, 
+//                 minute: DateTime.fromFormat(endTime, "hh:mm a").minute 
+//             });
+
+//             while (start < end) {
+//                 let slotEnd = start.plus({ minutes: sessionDuration });
+
+//                 schedules[key].slots.push({
+//                     startTime: start.toFormat("hh:mm a"),
+//                     endTime: slotEnd.toFormat("hh:mm a"),
+//                     isBooked: false
+//                 });
+
+//                 start = slotEnd;
+//             }
+//         }
+//     }
+
+    
+//     const availableSlots = Object.values(schedules);
+
+
+//     await setSchedule(availableSlots);
+//     return availableSlots;
+// };
