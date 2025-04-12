b54db8741429c4aa9f06ff5e194692e8b8d47d77
Akram Elgyar <125706128+AKRAMELGYAR@users.noreply.github.com>
2025-04-12 03:19:44 +0200

    fix messages

--- Auth/service/userSchema.js
+++ Auth/service/userSchema.js
@@ -3,11 +3,11 @@ import genralRules from "../../utils/generalRules/index.js";
 
 export const userSignUpSchema = {
     body: joi.object({
-        firstName: joi.string().min(3).max(15).required(),
-        lastName: joi.string().min(3).max(15).required(),
+        firstName: joi.string().min(3).max(15).message("firstName must br at least 3 characters").required(),
+        lastName: joi.string().min(3).max(15).message("lastName must be at least 3 characters").required(),
         userName: joi.string().min(3).max(15).required(),
         email: genralRules.email.message("invalid email").required(),
-        password: genralRules.password.required()
+        password: genralRules.password.message("password must contain at least 8 characters").required(),
     })
 }
 
